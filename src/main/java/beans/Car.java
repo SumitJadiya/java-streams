a6fd005a8c9495b77abcb4614007c6467476d8e6
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-02-22 17:32:02 +0530

    added beans

--- src/main/java/beans/Car.java
+++ src/main/java/beans/Car.java
@@ -1,4 +1,34 @@
 package beans;
 
+import lombok.Data;
+
+@Data
 public class Car {
+    private final Integer id;
+    private final String make;
+    private final String model;
+    private final String color;
+    private final Integer year;
+    private final Double price;
+
+    public Car(Integer id, String make, String model, String color, Integer year, Double price) {
+        this.id = id;
+        this.make = make;
+        this.model = model;
+        this.color = color;
+        this.year = year;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                "make=" + make +
+                "model=" + model +
+                "color=" + color +
+                "year=" + year +
+                "price=" + price +
+                "}";
+    }
 }
--- src/main/java/beans/Person.java
+++ src/main/java/beans/Person.java
@@ -1,4 +1,34 @@
 package beans;
 
+import lombok.Data;
+
+@Data
 public class Person {
+    final Integer id;
+    final String firstName;
+    final String lastName;
+    final String email;
+    final String gender;
+    final Integer age;
+
+    public Person(Integer id, String firstName, String lastName, String email, String gender, Integer age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", gender='" + gender + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
